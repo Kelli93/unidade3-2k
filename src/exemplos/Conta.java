945808f3a565256108f0fdc76c0367f2bfa02d6a
Kelli93 <167825011+Kelli93@users.noreply.github.com>
2024-05-03 20:53:51 -0300

    fiz um saque

--- src/exemplos/Conta.java
+++ src/exemplos/Conta.java
@@ -8,4 +8,12 @@ public class Conta {
     void depositar(double valor) {
         this.saldo = this.saldo + valor;
     }
+    boolean sacar(double Valor) {
+            if (this.saldo>=Valor){
+        this.saldo-=Valor;
+    return(true);
+}
+else
+      return false;
+    }
 }
--- src/exemplos/TesteConta.java
+++ src/exemplos/TesteConta.java
@@ -6,5 +6,10 @@ public class TesteConta {
         c.depositar(100);
         c.depositar(50);
         System.out.println("Saldo: " + c.saldo);
+        boolean saque_efetuado = c.sacar(120);
+        if (saque_efetuado)
+            System.out.println ("Saque Efetuado cam Sucesso");
+        else
+            System.out.println ("Saque não efetuado! Saldo insuficiente!");
     }
 }
