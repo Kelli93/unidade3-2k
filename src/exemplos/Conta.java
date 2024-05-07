023763f4082a7c895060a2580eab80bf60ddb67f
Kelli93 <167825011+Kelli93@users.noreply.github.com>
2024-05-06 21:48:30 -0300

    Me esforçando Professor !

--- src/exemplos/Conta.java
+++ src/exemplos/Conta.java
@@ -1,19 +1,37 @@
 package exemplos;
 
 public class Conta {
-    int numero;
-    String nomeTitular;
-    double saldo;
-    
-    void depositar(double valor) {
+
+    private int numero;
+    private String nomeTitular;
+    private double saldo;
+
+    public void depositar(double valor) {
         this.saldo = this.saldo + valor;
     }
-    boolean sacar(double Valor) {
-            if (this.saldo>=Valor){
-        this.saldo-=Valor;
-    return(true);
-}
-else
-      return false;
+
+    public boolean sacar(double Valor) {
+        if (this.saldo >= Valor) {
+            this.saldo -= Valor;
+            return (true);
+        } else 
+            return false;
+        
     }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+public int getNumero() {
+        return this.numero;
+}
+public String getNomeTitular() {
+    return this.nomeTitular;
+}
+public void setNumero(int numero) {
+    this.numero = numero;
+}
+public void setNomeTitular(String nomeTitular) {
+    this.nomeTitular = nomeTitular;
+}
 }
--- src/exemplos/TesteConta.java
+++ src/exemplos/TesteConta.java
@@ -5,11 +5,18 @@ public class TesteConta {
         Conta c = new Conta();
         c.depositar(100);
         c.depositar(50);
-        System.out.println("Saldo: " + c.saldo);
+     c.setNomeTitular("Kelli");
+     c.setNumero(2000);
+     
         boolean saque_efetuado = c.sacar(120);
-        if (saque_efetuado)
+        if (saque_efetuado){
             System.out.println ("Saque Efetuado cam Sucesso");
-        else
+    } else {
             System.out.println ("Saque não efetuado! Saldo insuficiente!");
+        
     }
+  System.out.println("Saldo:" + c.getSaldo());
+System.out.println("NomeTitular:"+ c.getNomeTitular());
+System.out.println("Numero:"+ c.getNumero());
+}
 }
